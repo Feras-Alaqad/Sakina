2ddd2737a32c588dc6d729b32714bbcfbbf350f3
Feras Alaqad <feras.kh.aqad@gmail.com>
2025-09-18 11:33:15 +0300

    feat: Add payment processing and email services, including client secret retrieval and email generation

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -4,9 +4,11 @@ import {
   findTherapistById, getAllTherapists, updateTherapistProfile, updateProfileImg,
 } from './therapists';
 
+import findClientSecret from './payment';
+
 import { adminLogin, getTherapistsForAdmin, updateTherapistActive } from './admin';
 
 
 export { login, getAuth, register, adminLogin, findTherapistById,
-    getAllTherapists, updateTherapistProfile,
+    getAllTherapists, updateTherapistProfile, findClientSecret,
     updateProfileImg, getTherapistsForAdmin, updateTherapistActive };
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,8 +1,13 @@
 import { loginByEmail, registerTherapist, registerUser } from './auth';
 import { getAdmin } from './admin';
 import { getTherapistById, getAllTherapist, updateTherapist } from './therapists';
+import createPresignedUrl from './S3Service';
+import mailer from './nodemailer';
+import generateEmail from './mailBuilder';
+import getClientSecret from './payment';
 
 export { loginByEmail, registerTherapist, registerUser, 
     getAdmin, getTherapistById, getAllTherapist, 
-    updateTherapist 
+    updateTherapist, createPresignedUrl, mailer, generateEmail,
+    getClientSecret
 };

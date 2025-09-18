9589407f68436b1894f0e41faa87269afd3cd7c3
Mohannad Alastal <90284916+MhndAs@users.noreply.github.com>
2025-09-18 11:55:15 +0300

    Complete merge and fix all service exports

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,9 +1,15 @@
 import { loginByEmail, registerTherapist, registerUser } from './auth';
 import { getAdmin } from './admin';
 import { getTherapistById, getAllTherapist, updateTherapist } from './therapists';
+import { getAppointmentsPerDateService, getAppointmentById, updateIsAvailable, addAppointment } from './appointments';
+import createPresignedUrl from './S3Service';
+import mailer from './nodemailer';
+import generateEmail from './mailBuilder';
+import getClientSecret from './payment';
 
 export { loginByEmail, registerTherapist, registerUser, 
     getAdmin, getTherapistById, getAllTherapist, 
-    updateTherapist, getAppointmentsPerDateService, getAppointmentById
-  , updateIsAvailable, addAppointment,
+    updateTherapist, getAppointmentsPerDateService, getAppointmentById,
+    updateIsAvailable, addAppointment, createPresignedUrl, mailer, generateEmail,
+    getClientSecret
 };
